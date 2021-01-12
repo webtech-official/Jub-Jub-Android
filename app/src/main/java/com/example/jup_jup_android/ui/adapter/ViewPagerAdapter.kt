fdb2f42ecb4e8a832a08cf00a7de16eb3dcf444a
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-12 17:24:58 +0900

    Create ViewPagerAdapter

--- app/src/main/java/com/example/jup_jup_android/ui/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/jup_jup_android/ui/adapter/ViewPagerAdapter.kt
@@ -1,4 +1,50 @@
 package com.example.jup_jup_android.ui.adapter
 
-class ViewPagerAdapter {
+import android.content.Context
+import android.util.Log
+import androidx.viewpager.widget.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.jup_jup_android.R
+import com.example.jup_jup_android.entity.singleton.ItemStatusListManager
+import kotlinx.android.synthetic.main.fragment_item_status_list_1.*
+import kotlinx.android.synthetic.main.fragment_item_status_list_1.view.*
+
+import java.util.*
+class ViewPagerAdapter(context: Context) : PagerAdapter() {
+    private var context = context
+    private lateinit var layoutInflater: LayoutInflater
+
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object` as View
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = layoutInflater.inflate(R.layout.fragment_item_status_list_1, null)
+        val reserveCal: Calendar = Calendar.getInstance()
+
+        var adapter = ItemStatusList_RecyclerViewAdpater(view.context, position)
+        view.recyclerView_ItemStatusList.recyclerView_ItemStatusList.adapter = adapter
+        adapter.notifyDataSetChanged()
+
+        //setTextViewsText(view, reserveCal, position)
+        container.addView(view)
+        Log.d("TestLog", "ViewPagerView Return")
+        return view
+    }
+
+
+
+
 }
