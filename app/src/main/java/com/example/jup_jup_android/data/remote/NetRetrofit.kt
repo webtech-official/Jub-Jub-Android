670f102d3c6c532feab0c85277aedcdbb09de5db
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-13 15:23:37 +0900

    Retrofit 객체 생성

--- app/src/main/java/com/example/jup_jup_android/data/remote/NetRetrofit.kt
+++ app/src/main/java/com/example/jup_jup_android/data/remote/NetRetrofit.kt
@@ -1,4 +1,29 @@
 package com.example.jup_jup_android.data.remote
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 object NetRetrofit {
+
+    private val BASE_URL = "http://192.168.0.107:8080/api/v1/"
+
+    fun getServiceApi() : Api {
+
+        val interceptor = HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        var retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        return retrofit.create(Api::class.java)
+    }
+
 }
