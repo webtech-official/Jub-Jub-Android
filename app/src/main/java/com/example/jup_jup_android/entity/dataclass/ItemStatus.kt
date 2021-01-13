dcf050e1cae32fb06635040d70ebc89ab9c25341
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-13 22:48:52 +0900

    ItemStatus Element Key 수정

--- app/src/main/java/com/example/jup_jup_android/entity/dataclass/ItemStatus.kt
+++ app/src/main/java/com/example/jup_jup_android/entity/dataclass/ItemStatus.kt
@@ -1,10 +1,11 @@
 package com.example.jup_jup_android.entity.dataclass
 
 import android.graphics.Bitmap
+import java.io.Serializable
 
 data class ItemStatus(
-        var bitmap: Bitmap,
+        var itemImage: Bitmap,
         var itemName: String,
         var itemCategory: String,
         var itemCount: Int
-)
+) : Serializable
