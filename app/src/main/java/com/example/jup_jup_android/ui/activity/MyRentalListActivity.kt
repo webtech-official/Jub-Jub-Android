1e7bbbf0dd6a681261037de4d75af8042286a3f0
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-13 15:25:40 +0900

    오류 수정

--- app/src/main/java/com/example/jup_jup_android/ui/activity/MyRentalListActivity.kt
+++ app/src/main/java/com/example/jup_jup_android/ui/activity/MyRentalListActivity.kt
@@ -10,19 +10,27 @@ import androidx.viewpager.widget.ViewPager
 import com.example.jup_jup_android.R
 import com.example.jup_jup_android.entity.singleton.ItemStatusListManager
 import com.example.jup_jup_android.ui.adapter.ViewPagerAdapter
+import com.example.jup_jup_android.ui.util.SetPageView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_my_rental_list.*
 import kotlinx.android.synthetic.main.layout_pageview.view.*
 
 class MyRentalListActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_rental_list)
+
+        SetPageView(applicationContext, pageView_MyRentalListActivity, ItemStatusListManager.getDevidedItemStatusList()).initViewPager()
+
+        setTitleBarItemsOnclick()
     }
 
 
     private fun setTitleBarItemsOnclick() {
 
-        imageView_MyPage_MainActivity.setOnClickListener {
+        imageView_BackArrow_MyRentalActivity.setOnClickListener {
             startActivity(Intent(applicationContext, MyPageActivity::class.java))
         }
     }
