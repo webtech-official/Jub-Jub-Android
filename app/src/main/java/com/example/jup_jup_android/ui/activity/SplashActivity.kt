9d12bcbf3dc4c31bed0ba034e82048ab82e2c815
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-13 22:52:02 +0900

    ?

--- app/src/main/java/com/example/jup_jup_android/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/jup_jup_android/ui/activity/SplashActivity.kt
@@ -3,8 +3,14 @@ package com.example.jup_jup_android.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.jup_jup_android.R
+import com.example.jup_jup_android.data.remote.NetRetrofit
+import com.example.jup_jup_android.entity.dataclass.response.ResponseTest
 import com.example.jup_jup_android.entity.singleton.ItemStatusListManager
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.lang.Thread.sleep
 
 class SplashActivity : AppCompatActivity() {
@@ -19,6 +25,26 @@ class SplashActivity : AppCompatActivity() {
     private fun startApp() {
         ItemStatusListManager.initItemStatusList(applicationContext, 100)
 
+        val response: Call<ResponseTest> = NetRetrofit.getServiceApi().getTest()
+
+        response.enqueue(object: Callback<ResponseTest>{
+            override fun onResponse(call: Call<ResponseTest>, response: Response<ResponseTest>) {
+                Log.d("TestLog", "onResponse!")
+                if(response.code() == 200){
+                    Log.d("TestLog", "body.msg = ${response.body()?.msg}")
+
+                }else{
+                    Log.d("TestLog", "${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseTest>, t: Throwable) {
+                Log.d("TestLog", t.message.toString())
+                Log.d("TestLog", "Fail!")
+            }
+        })
+
         startActivity(Intent(applicationContext, LoginActivity ::class.java))
+        finish()
     }
 }
