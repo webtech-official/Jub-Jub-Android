42c5630cf056a7b60d1ef1a96bfd06677874d741
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-13 10:44:20 +0900

    타이틀 바 온클릭 추가

--- app/src/main/java/com/example/jup_jup_android/ui/activity/MyRentalListActivity.kt
+++ app/src/main/java/com/example/jup_jup_android/ui/activity/MyRentalListActivity.kt
@@ -1,12 +1,31 @@
 package com.example.jup_jup_android.ui.activity
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.viewpager.widget.ViewPager
 import com.example.jup_jup_android.R
+import com.example.jup_jup_android.entity.singleton.ItemStatusListManager
+import com.example.jup_jup_android.ui.adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_pageview.view.*
 
 class MyRentalListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_rental_list)
     }
+
+
+    private fun setTitleBarItemsOnclick() {
+
+        imageView_MyPage_MainActivity.setOnClickListener {
+            startActivity(Intent(applicationContext, MyPageActivity::class.java))
+        }
+    }
+
+
 }
