52f9539e28fdbeb68ebc63aad1e87b418abce6a9
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-14 15:31:34 +0900

    RecyclerViewAdpater 매개변수 수정 / RentActivity용 로직 추가

--- app/src/main/java/com/example/jup_jup_android/ui/adapter/ItemStatusList_RecyclerViewAdpater.kt
+++ app/src/main/java/com/example/jup_jup_android/ui/adapter/ItemStatusList_RecyclerViewAdpater.kt
@@ -1,99 +1,99 @@
 package com.example.jup_jup_android.ui.adapter
 
-import android.content.Context
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jup_jup_android.R
 import com.example.jup_jup_android.entity.dataclass.ItemStatus
-import com.example.jup_jup_android.entity.singleton.ItemStatusListManager
+import com.example.jup_jup_android.entity.dataclass.RentStatus
 import com.example.jup_jup_android.ui.activity.RentActivity
+import com.example.jup_jup_android.ui.util.MyUtil
 import kotlinx.android.synthetic.main.layout_equipmentlist_item.view.*
 
-class ItemStatusList_RecyclerViewAdpater (val context: Context, pPosition: Int):RecyclerView.Adapter<ItemStatusList_RecyclerViewAdpater.ViewHolder>() {
-    var pagePosition = pPosition
-    private var devideditemStatusList = ItemStatusListManager.getDevidedItemStatusList()[pagePosition]
+class ItemStatusList_RecyclerViewAdpater (dataList: ArrayList<ItemStatus>):RecyclerView.Adapter<ItemStatusList_RecyclerViewAdpater.ViewHolder>() {
 
-    fun getDataList(position: Int): ArrayList<ItemStatus> {
+    private var devideditemStatusList = dataList
 
-        var dataList = ItemStatusListManager.getItemStatusList()
-        Log.d("TestLog","${dataList}")
-        if(position == dataList.size/5){
-            Log.d("TestLog","${dataList.subList(position*5, dataList.size)}")
-            return dataList.subList(position*5, dataList.size) as ArrayList<ItemStatus>
-
-        }else{
-            Log.d("TestLog","${dataList.subList(position*5, position*5 + 4)}")
-            return dataList.subList(position*5, position*5 + 4) as ArrayList<ItemStatus>
-        }
-
-    }
-
-    //var position : Int = 0
-    //아이템의 갯수
     override fun getItemCount(): Int {
         return devideditemStatusList.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemStatusList_RecyclerViewAdpater.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.layout_equipmentlist_item, parent, false)
         return ViewHolder(v)
     }
 
-    //this method is binding the data on the list
-    override fun onBindViewHolder(holder: ItemStatusList_RecyclerViewAdpater.ViewHolder, itemPosition: Int) {
-        holder.bindItems(devideditemStatusList[itemPosition], pagePosition, itemPosition)
-        //this.position = position
+    override fun onBindViewHolder(holder: ViewHolder, itemPosition: Int) {
+        holder.bindItems(devideditemStatusList[itemPosition])
     }
 
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        fun bindItems(data: ItemStatus, pagePosition: Int, itemPosition: Int){
-            //이미지표시
+        fun bindItems(data: ItemStatus){
 
-//            Glide.with(itemView.context).load(data.imageResource)
-//                .into(itemView.recyclerView_ImageView)
-            itemView.imageView_ItemImage_EquipmentList_Item.setImageBitmap(data.itemImage)
-            itemView.textView_ItemName_EquipmentList_Item.text = data.itemName
-            itemView.textView_ItemCategory_EquipmentList_Item.text = data.itemCategory
-            itemView.textView_ItemCount_EquipmentList_Item.text = "수량 : ${data.itemCount}개"
-
+            setTextViewsText(data)
 
             //각각의 아이템 클릭시
-
             itemView.setOnClickListener {
-                //여기서 토스터를 어떻게?
-                //Toast.makeText(itemView.context, "아이템 '${data.name}'를 클릭했습니다.", Toast.LENGTH_LONG).show()
-                makeToastitemView(itemView)
-//                val intent = Intent(itemView.context,::class.java)
-//                intent.putExtra("field", data.name)
-//                itemView.context.startActivity(intent)
+
                 var intent = Intent(itemView.context, RentActivity::class.java)
-                intent.putExtra("PageIndex", pagePosition)
-                intent.putExtra("ItemIndex", itemPosition)
+
+                intent.putExtra("Data", data)
                 intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+
                 itemView.context.startActivity(intent)
             }
+        }
 
+        fun bindItems(data: RentStatus){
+            setTextViewsText(data)
+
+            //각각의 아이템 클릭시
+            itemView.setOnClickListener {
+
+                var intent = Intent(itemView.context, RentActivity::class.java)
+
+                intent.putExtra("Data", data)
+                intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+
+                itemView.context.startActivity(intent)
+            }
+        }
+
+
+        private fun setTextViewsText(data: ItemStatus) {
+            itemView.imageView_ItemImage_Item.setImageBitmap(MyUtil().convertBase64ToBitmap(data.image))
+            itemView.textView_ItemName_Item.text = data.name
+            itemView.textView_ItemCategory_Item.text = data.category
+            itemView.textView_ItemCount_Item.text = "수량 : ${data.count}개"
 
         }
 
-        private fun makeToastitemView(itemView: View){
-            Log.d("TestLog", "position = $position 를 클릭했습니다.")
-            Log.d("TestLog", "adapterPosition = $adapterPosition 를 클릭했습니다.")
-            Log.d("TestLog", "layoutPosition = $layoutPosition 를 클릭했습니다.")
-            Log.d("TestLog", "oldPosition = $oldPosition 를 클릭했습니다.")
+        private fun setTextViewsText(data: RentStatus) {
+            itemView.imageView_ItemImage_Item.setImageBitmap(MyUtil().convertBase64ToBitmap(data.image))
+            itemView.textView_ItemName_Item.text = data.name
+            itemView.textView_ItemCategory_Item.text = data.category
+            itemView.textView_ItemCount_Item.text = "수량 : ${data.count}개"
 
+            itemView.textView_RentStatus_Item.visibility = View.VISIBLE
+            itemView.textView_RentStatus_Item.text = data.status
 
+            when(data.status){
+                "반납" -> setRentStatus(itemView, R.drawable.round_black_back_white_edge, R.color.white)
+                "대여" -> setRentStatus(itemView, R.drawable.round_yellow_back_yellow_edge, R.color.black)
+                "연체" -> setRentStatus(itemView, R.drawable.round_white_back_white_edge, R.color.black)
+                else -> itemView.textView_RentStatus_Item.text = "?"
+
+            }
         }
 
-
+        private fun setRentStatus(view: View, backgroundId: Int, textColorId: Int){
+            itemView.textView_RentStatus_Item.background = ContextCompat.getDrawable(view.context, backgroundId)
+            itemView.textView_RentStatus_Item.setTextColor(ContextCompat.getColor(view.context, textColorId))
+        }
     }
-
-
-
 }
