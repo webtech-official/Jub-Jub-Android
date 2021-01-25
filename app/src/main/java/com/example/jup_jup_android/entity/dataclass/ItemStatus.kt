b7c88d6abfb89afea72ab55acac0544f9c01ae0a
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-25 20:42:05 +0900

    id의 Data Type Long -> Int

--- app/src/main/java/com/example/jup_jup_android/entity/dataclass/ItemStatus.kt
+++ app/src/main/java/com/example/jup_jup_android/entity/dataclass/ItemStatus.kt
@@ -1,14 +1,12 @@
 package com.example.jup_jup_android.entity.dataclass
 
-import android.graphics.Bitmap
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
-import java.util.*
 
 @Entity(tableName = "itemStatus")
 data class ItemStatus(
-        @PrimaryKey var id: Long,
+        @PrimaryKey var id: Int,
         var image: String,
         var name: String,
         var category: String,
