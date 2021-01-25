a0b684bf690f5d20e8eebbe8be8eefee6bf5a071
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-25 16:07:39 +0900

    Update dataClass -> Entity

--- app/src/main/java/com/example/jup_jup_android/entity/dataclass/ItemStatus.kt
+++ app/src/main/java/com/example/jup_jup_android/entity/dataclass/ItemStatus.kt
@@ -1,11 +1,14 @@
 package com.example.jup_jup_android.entity.dataclass
 
 import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.io.Serializable
 import java.util.*
 
+@Entity(tableName = "itemStatus")
 data class ItemStatus(
-        var id: Long,
+        @PrimaryKey var id: Long,
         var image: String,
         var name: String,
         var category: String,
