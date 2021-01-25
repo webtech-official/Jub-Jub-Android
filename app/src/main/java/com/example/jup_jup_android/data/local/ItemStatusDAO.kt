b4d8163682e04ba8d2946fcfc02fc828a1fdc041
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-25 20:41:26 +0900

    ItemStatus DAO 생성

--- app/src/main/java/com/example/jup_jup_android/data/local/ItemStatusDAO.kt
+++ app/src/main/java/com/example/jup_jup_android/data/local/ItemStatusDAO.kt
@@ -1,4 +1,22 @@
 package com.example.jup_jup_android.data.local
 
+import androidx.room.*
+import com.example.jup_jup_android.entity.dataclass.ItemStatus
+
+@Dao
 interface ItemStatusDAO {
+
+    @Query("SELECT * FROM itemStatus")
+    fun getAll(): List<ItemStatus>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(itemStatus: ItemStatus)
+
+
+    @Query("SELECT * FROM itemStatus WHERE name || category  LIKE :word ")
+    fun search(word: String): List<ItemStatus>
+
+    @Query("DELETE from itemStatus")
+    fun clear()
+
 }
