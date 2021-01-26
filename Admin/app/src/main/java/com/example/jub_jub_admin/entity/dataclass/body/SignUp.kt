9fe32c969572db2deb67ab24eea94d9bf7e9a79d
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-27 00:27:54 +0900

    Revert Change

--- Admin/app/src/main/java/com/example/jub_jub_admin/entity/dataclass/body/SignUp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jup_jup_android.entity.dataclass.body
-
-data class SignUp(
-    val classNumber: String,
-    val email: String,
-    val name: String,
-    val password: String
-)
--- Admin/app/src/main/java/com/example/jub_jub_admin/entity/dataclass/response/LoginResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jup_jup_android.entity.dataclass.response
-
-data class LoginResponse (
-    val email: String,
-    val classNumber: String,
-    val token: String,
-    val msg: String = ""
-)
