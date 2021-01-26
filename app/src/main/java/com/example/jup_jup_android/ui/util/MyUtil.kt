f1abb27ca45d4edd8949414c6f7f1c3a32052683
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-26 17:17:52 +0900

    어림도 없지 똑같은 에러 뜨는데 대체 왜 그런지 모르겠다..

--- app/src/main/java/com/example/jup_jup_android/ui/util/MyUtil.kt
+++ app/src/main/java/com/example/jup_jup_android/ui/util/MyUtil.kt
@@ -11,25 +11,11 @@ import java.io.ByteArrayOutputStream
 
 
 class MyUtil {
-    var baseString = ""
 
     fun convertBase64ToBitmap(base64Code: String): Bitmap{
 
         val decodedString: ByteArray = Base64.decode(base64Code, Base64.DEFAULT)
         return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
-    }
-
-    fun setBaseString(context: Context){
-        val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.imageex)
-        val byteStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream)
-        val byteArray: ByteArray = byteStream.toByteArray()
-        baseString = Base64.encodeToString(byteArray, Base64.DEFAULT)
-    }
-
-    @JvmName("getBaseString1")
-    fun getBaseString(): String {
-        return baseString
     }
 
 
