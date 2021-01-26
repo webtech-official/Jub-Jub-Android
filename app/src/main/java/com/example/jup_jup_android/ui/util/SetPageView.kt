57eeb9eafdc5870667290976be79d4623e2b0997
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-26 15:26:48 +0900

    ?

--- app/src/main/java/com/example/jup_jup_android/ui/util/SetPageView.kt
+++ app/src/main/java/com/example/jup_jup_android/ui/util/SetPageView.kt
@@ -94,8 +94,7 @@ class SetPageView(val view: View, private val viewPager: ViewPager, private val 
         textHighlightOff()
         textHighlightOn(destination)
         viewPager.setCurrentItem(destination, true)
-        //viewPager.currentItem =
-        //lastPage = destination
+
     }
 
     // 존재하지 않는 페이지의 숫자는 GONE으로, 존재하는데 보이지 않는 페이지는 VISIBLE로
