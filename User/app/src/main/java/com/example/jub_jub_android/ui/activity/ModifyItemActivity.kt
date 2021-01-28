32099f333594cb38e5c52fae0896c5ca6dd6bcc6
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-28 19:34:18 +0900

    Modify 기자재 관리 액티비티

--- User/app/src/main/java/com/example/jub_jub_android/ui/activity/ModifyItemActivity.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/activity/ModifyItemActivity.kt
@@ -12,17 +12,24 @@ import com.example.jub_jub_android.entity.dataclass.ItemStatus
 import com.example.jub_jub_android.ui.util.MyUtil
 import kotlinx.android.synthetic.main.activity_modify_item.*
 
-class ModifyItemActivity : AppCompatActivity() {
+class ModifyItemActivity : AppCompatActivity(){
+
+    var isModify = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modify_item)
 
+        if(intent.hasExtra("Data")){
+            isModify = true
+            modifyMode(intent.getSerializableExtra("Data") as ItemStatus)
+        }
         init()
-
     }
 
     private fun init() {
-        val data: ItemStatus = intent.getSerializableExtra("Data") as ItemStatus
+
         imageView_OpenGallery_ModifyItemActivity.setOnClickListener {
             openGallery()
         }
@@ -30,10 +37,16 @@ class ModifyItemActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+
+    private fun modifyMode(data: ItemStatus){
         imageView_ItemImage_ModifyActivity.setImageBitmap(MyUtil().convertBase64ToBitmap(data.image))
         editText_ItemName_ModifyItemActivity.setText(data.name)
         editText_ItemCount_ModifyItemActivity.setText(data.count.toString())
         editText_ItemCategory_ModifyItemActivity.setText(data.category)
+
+        textView_ModifyMode_ModifyItemActivity.text = "수정"
+        button_Modify_ModifyItemActivity.text = "수정"
     }
 
     private fun openGallery() {
