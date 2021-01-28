66b92ba2bb331b0460eb4a98a5733278f0145cc6
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-28 17:02:37 +0900

    노트북 관리 DAO 생성

---
+++ User/app/src/main/java/com/example/jub_jub_android/data/local/LaptopStatusDAO.kt
@@ -0,0 +1,25 @@
+package com.example.jub_jub_android.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.jub_jub_android.entity.dataclass.LaptopStatus
+
+
+@Dao
+interface LaptopStatusDAO {
+
+        @Query("SELECT * FROM laptopStatus")
+        fun getAll(): List<LaptopStatus>
+
+        @Query("SELECT * FROM laptopStatus WHERE name LIKE :word ")
+        fun search(word: String): List<LaptopStatus>
+
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        fun insert(laptopStatus: LaptopStatus)
+
+        @Query("DELETE from laptopStatus")
+        fun clear()
+
+}
