1519ab28694e5520dec47a5c9588058dd1ad8b70
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-28 17:02:48 +0900

    노트북 관리 DB

---
+++ User/app/src/main/java/com/example/jub_jub_android/data/local/LaptopStatusDB.kt
@@ -0,0 +1,34 @@
+package com.example.jub_jub_android.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.jub_jub_android.entity.dataclass.LaptopStatus
+
+
+@Database(entities = [LaptopStatus::class], version = 1)
+abstract class LaptopStatusDB: RoomDatabase() {
+
+    abstract fun laptopStatusDAO(): LaptopStatusDAO
+
+    companion object {
+        private var INSTANCE: LaptopStatusDB? = null
+
+        fun getInstance(context: Context): LaptopStatusDB? {
+            if (INSTANCE == null) {
+                synchronized(LaptopStatusDB::class) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                        LaptopStatusDB::class.java, "laptopStatus.db")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+
+    }
+}
