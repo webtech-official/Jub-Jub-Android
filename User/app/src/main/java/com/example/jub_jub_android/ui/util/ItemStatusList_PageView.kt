21a4f1bef877a897ff35f55464bf14fd269ecd9b
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-28 14:52:36 +0900

    패키지 명 변경

---
+++ User/app/src/main/java/com/example/jub_jub_android/ui/util/ItemStatusList_PageView.kt
@@ -0,0 +1,34 @@
+package com.example.jub_jub_android.ui.util
+
+import android.content.Context
+import android.view.View
+import com.example.jub_jub_android.entity.dataclass.ItemStatus
+import com.example.jub_jub_android.entity.singleton.ItemStatusListManager
+import com.example.jub_jub_android.ui.adapter.ItemStatusList_ViewPagerAdapter
+import kotlinx.android.synthetic.main.layout_pageview.view.*
+
+class ItemStatusList_PageView(var context: Context, var view: View, var  dataList: ArrayList<ArrayList<ItemStatus>>){
+
+    private lateinit var setPageView : SetPageView
+
+    fun initViewPager() {
+
+        val viewPager = view.viewPager
+
+        viewPager.adapter = ItemStatusList_ViewPagerAdapter(context)
+
+        setPageView = SetPageView(view, viewPager, dataList as ArrayList<ArrayList<Any>>)
+
+    }
+
+    fun syncPage(){
+        //Log.d("TestLog", "ISList.size = ${ItemStatusListManager.getShowList().size}")
+        setPageView.syncPage(ItemStatusListManager.getShowList().size)
+    }
+
+
+
+
+
+}
+
--- User/app/src/main/java/com/example/jub_jub_user/entity/dataclass/body/SignUp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jub_jub_user.entity.dataclass.body
-
-data class SignUp(
-    val classNumber: String,
-    val email: String,
-    val name: String,
-    val password: String
-)
