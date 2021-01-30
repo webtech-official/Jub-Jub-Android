930c65f31efbd75cecc44cb8031738ec59c77846
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-30 20:48:26 +0900

    MyUtil 클래스에 샘플 이미지 src -> base64 변환 후 리턴, 페이징 처리, DialogBase 생성 메서드 추가

--- User/app/src/main/java/com/example/jub_jub_android/ui/util/MyUtil.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/util/MyUtil.kt
@@ -1,8 +1,14 @@
 package com.example.jub_jub_android.ui.util
 
+import android.app.Dialog
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
+import android.view.Window
+import com.example.jub_jub_android.R
+import kotlinx.android.synthetic.main.layout_alertdialog.*
+import java.io.ByteArrayOutputStream
 
 
 class MyUtil {
@@ -11,6 +17,45 @@ class MyUtil {
 
         val decodedString: ByteArray = Base64.decode(base64Code, Base64.DEFAULT)
         return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+    }
+
+    fun getMotorTestImage(context: Context): String{
+        val byteStream = ByteArrayOutputStream()
+        val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.imageex)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream)
+        val byteArray: ByteArray = byteStream.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
+    }
+
+    fun processShowList(dataList: ArrayList<Any>, key: String, showList: ArrayList<ArrayList<Any>>){
+
+        showList.clear()
+        var page = 0
+        var cnt = 0
+        showList.add(ArrayList())
+
+        for (i in 0 until dataList.size) {
+            if (cnt == 5) {
+                showList.add(ArrayList())
+                cnt = 0
+                page++
+            }
+            showList[page].add(dataList[i])
+            cnt++
+        }
+    }
+
+    fun makeBaseDialog(context: Context, dialogName: String): Dialog {
+        var dialog = Dialog(context)
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.layout_alertdialog)
+
+        dialog.textView_AlertName_AlertDialogLayout.text = dialogName
+        dialog.textView_AlertContent_AlertDialogLayout.text = "정말 $dialogName 하시겠습니까?"
+
+        return dialog
+
     }
 
 
