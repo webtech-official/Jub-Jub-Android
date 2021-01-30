828ca405ff12d5aba3e1b84e218c82033ffb4653
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-01-30 20:24:49 +0900

    전체 대여 기록 DAO 생성

---
+++ User/app/src/main/java/com/example/jub_jub_android/data/local/DAO/RentRecordDAO.kt
@@ -0,0 +1,24 @@
+package com.example.jub_jub_android.data.local.DAO
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.jub_jub_android.entity.dataclass.RentRecord
+
+@Dao
+interface RentRecordDAO {
+
+    @Query("SELECT * FROM rentRecord")
+    fun getAll(): List<RentRecord>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(rentRecord: RentRecord)
+
+    @Query("SELECT * FROM rentRecord WHERE name || category || studentNumber || date LIKE :word ")
+    fun search(word: String): List<RentRecord>
+
+    @Query("DELETE from rentRecord")
+    fun clear()
+
+}
