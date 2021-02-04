f8088dc53468104bf2048d7d48cf0a476b30df02
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-04 20:47:51 +0900

    기자재 등록 메서드 추가

--- User/app/src/main/java/com/example/jub_jub_android/data/remote/Api.kt
+++ User/app/src/main/java/com/example/jub_jub_android/data/remote/Api.kt
@@ -3,10 +3,12 @@ package com.example.jub_jub_android.data.remote
 import com.example.jub_jub_android.entity.dataclass.body.Login
 import com.example.jub_jub_android.entity.dataclass.body.SignUp
 import com.example.jub_jub_android.entity.dataclass.response.LoginResponse
+import com.example.jub_jub_android.entity.dataclass.response.MyResponse
 import com.example.jub_jub_android.entity.dataclass.response.ResponseTest
 import com.example.jub_jub_android.entity.dataclass.response.SignUpResponse
 import retrofit2.Call
 import retrofit2.http.*
+import java.io.File
 
 
 interface Api {
@@ -19,5 +21,14 @@ interface Api {
 
     @POST("signup")
     fun signUp(@Body signUp: SignUp): Call<SignUpResponse>
+
+    @POST("equipment")
+    fun addItem(@Header("X-AUTH-TOKEN") token: String,
+                @Query("img_equipment") img_equipment: File,
+                @Query("name") name: String,
+                @Query("content") content: String,
+                @Query("count") count: Int,
+
+                ): Call<MyResponse>
 
 }
