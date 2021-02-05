6dad99af3371252c2a014abbb4f8593b176e43ea
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-05 16:02:25 +0900

    .

--- User/app/src/main/java/com/example/jub_jub_android/ui/activity/MainActivity.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/activity/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-+
+
         pageView = ItemStatusList_PageView(applicationContext, pageView_MainActivity, ItemStatusListManager.getShowList())
         pageView.initViewPager()
 
