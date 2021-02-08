f7e500a25dee64ac74d692893948cddf453b4da6
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-08 16:15:37 +0900

    테스트용 로그

--- User/app/src/main/java/com/example/jub_jub_android/ui/util/MyUtil.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/util/MyUtil.kt
@@ -3,12 +3,13 @@ package com.example.jub_jub_android.ui.util
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
+import android.util.Log
 
 
 class MyUtil {
 
     fun convertBase64ToBitmap(base64Code: String): Bitmap{
-
+        Log.d("TestLog_MyUtil", "${base64Code}")
         val decodedString: ByteArray = Base64.decode(base64Code, Base64.DEFAULT)
         return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
     }
