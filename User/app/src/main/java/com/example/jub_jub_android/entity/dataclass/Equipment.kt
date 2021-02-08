ed37f590d378343f15a3cf2a6427b575196cc036
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-08 16:12:24 +0900

    데이터 클래스 이름 변경

---
+++ User/app/src/main/java/com/example/jub_jub_android/entity/dataclass/Equipment.kt
@@ -0,0 +1,18 @@
+package com.example.jub_jub_android.entity.dataclass
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+@Entity(tableName = "itemStatus")
+data class Equipment(
+        @PrimaryKey(autoGenerate = true) var id: Int,
+        @SerializedName("content") var category: String,
+        var count: Int,
+        @SerializedName("img_equipment") var image: String,
+        var name: String
+
+) : Serializable{
+    constructor(category: String, count: Int, image: String, name: String): this(0, category, count, image, name)
+}
