0aa0073f1714e975d1ac315a44370db6de58a247
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-08 16:15:32 +0900

    클래스 이름 변경

--- User/app/src/main/java/com/example/jub_jub_android/ui/util/ItemStatusList_PageView.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/util/ItemStatusList_PageView.kt
@@ -2,12 +2,12 @@ package com.example.jub_jub_android.ui.util
 
 import android.content.Context
 import android.view.View
-import com.example.jub_jub_android.entity.dataclass.ItemStatus
+import com.example.jub_jub_android.entity.dataclass.Equipment
 import com.example.jub_jub_android.entity.singleton.ItemStatusListManager
 import com.example.jub_jub_android.ui.adapter.ItemStatusList_ViewPagerAdapter
 import kotlinx.android.synthetic.main.layout_pageview.view.*
 
-class ItemStatusList_PageView(var context: Context, var view: View, var  dataList: ArrayList<ArrayList<ItemStatus>>){
+class ItemStatusList_PageView(var context: Context, var view: View, var  dataList: ArrayList<ArrayList<Equipment>>){
 
     private lateinit var setPageView : SetPageView
 
