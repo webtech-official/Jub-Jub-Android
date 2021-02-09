960bb2dd456071ef2ba403068ae97334f08b8f4f
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-09 23:46:23 +0900

    나의 기자재 Response Data Class들 생성

--- User/app/src/main/java/com/example/jub_jub_android/entity/dataclass/response/MyEquipmentResponse.kt
+++ User/app/src/main/java/com/example/jub_jub_android/entity/dataclass/response/MyEquipmentResponse.kt
@@ -1,18 +1,33 @@
 package com.example.jub_jub_android.entity.dataclass.response
 
-import com.example.jub_jub_android.entity.dataclass.Equipment
+import com.google.gson.annotations.SerializedName
+import java.io.File
+import java.sql.Timestamp
+import kotlin.collections.ArrayList
 
 data class MyEquipmentResponse (
+        var success: Boolean,
         var code: Int,
-        var list: ArrayList<DetailInfoOfMyequipment>,
         var msg: String,
-        var success: Boolean
+        var list: ArrayList<MyEquipmentDetailInfo>
 )
 
-data class DetailInfoOfMyequipment(
-        var admin: ArrayList<IDontKnowWhatisit>
+data class MyEquipmentDetailInfo(
+        var createdDate: String,
+        var modifiedDate: String,
+        var eqa_Idx: Int,
+        var amount: Int,
+        var reason: String,
+        var equipmentEnum: String,
+        var isReturn: Boolean?,
+        var equipment: MyEquipmentFromServer
 )
 
-class IDontKnowWhatisit {
+data class MyEquipmentFromServer(
+        var equ_Idx: Int,
+        var name: String,
+        var content: String,
+        var count: Int,
+        var img_equipment: String
+)
 
-}
