6f019f1e7929d608f8ad29ec9b67723fb69d7bbb
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-09 23:45:32 +0900

    Data Class 이름 변경 RentStatus -> MyEquipment

--- User/app/src/main/java/com/example/jub_jub_android/entity/dataclass/MyEquipment.kt
+++ User/app/src/main/java/com/example/jub_jub_android/entity/dataclass/MyEquipment.kt
@@ -4,16 +4,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
 
-@Entity(tableName = "rentStatus")
-data class RentStatus(
+@Entity(tableName = "myEquipment")
+data class MyEquipment(
         @PrimaryKey(autoGenerate = true) val rentId: Int,
-        var userId: String,
         var name: String,
         var category: String,
         var count: Int,
         var image: String,
         var status: String
 ): Serializable{
-    constructor(userId: String, name: String, category: String, count: Int, image: String, status: String) : this(0, userId, name, category, count, image, status)
+    constructor(name: String, category: String, count: Int, image: String, status: String) : this(0, name, category, count, image, status)
 }
 
---
+++ User/app/src/main/java/com/example/jub_jub_android/entity/singleton/MyEquipmentListManager.kt
@@ -0,0 +1,93 @@
+package com.example.jub_jub_android.entity.singleton
+
+import android.content.Context
+import android.util.Log
+import com.example.jub_jub_android.data.local.MyEquipmentDB
+import com.example.jub_jub_android.entity.dataclass.MyEquipment
+import com.example.jub_jub_android.entity.dataclass.response.MyEquipmentDetailInfo
+import com.example.jub_jub_android.ui.util.MyUtil
+
+object MyEquipmentListManager {
+
+    private lateinit var myEquipmentDB : MyEquipmentDB
+
+    private var dividedShowList = ArrayList<ArrayList<MyEquipment>>()
+
+    fun setDataList(context: Context, dataList: ArrayList<MyEquipmentDetailInfo>){
+        myEquipmentDB = MyEquipmentDB.getInstance(context)!!
+
+
+        val thread = Thread(Runnable {
+            myEquipmentDB.myEquipmentDAO().clear()
+
+            for(i in 0 until dataList.size){
+//            constructor(name: String, category: String, count: Int, image: String, status: String) : this(0, name, category, count, image, status)
+                var data = dataList[i].equipment
+
+                myEquipmentDB.myEquipmentDAO().insert(MyEquipment(data.name, data.content, data.count,
+                    MyUtil().convertFileToBase64(data.img_equipment)!!, getStatus(dataList[i].equipmentEnum)))
+                Log.d("TestLog_MyEqManager", "$i = ${MyUtil().convertFileToBase64(data.img_equipment)!!}")
+            }
+
+            Log.d("TestLog_MyEqManager", "데이터 입력 완료")
+        })
+
+        thread.start()
+
+        try {
+            thread.join()
+        } catch (e:InterruptedException){Log.d("TestLog_MyEqManager", "데이터 입력 에러 발생 ${e.message}")}
+
+        processShowList("")
+    }
+
+    private fun getStatus(status: String): String {
+        return when(status){
+            "ROLE_Waiting" -> "대여"
+            //대여, 반납, 연체 추가
+            else -> "?"
+        }
+    }
+
+    fun processShowList(key: String){
+        var dataList = ArrayList<MyEquipment>()
+
+        var r = Runnable {
+
+            if(key == "전체"){
+                dataList = myEquipmentDB.myEquipmentDAO().getAll() as ArrayList<MyEquipment>
+                Log.d("TestLog", " rent withoutkey ${dataList.size}")
+            }
+            else {
+                dataList = myEquipmentDB.myEquipmentDAO().search("%$key%") as ArrayList<MyEquipment>
+                Log.d("TestLog", " rent $key = ${dataList.size}")
+            }
+
+            dividedShowList.clear()
+            var page = 0
+            var cnt = 0
+            dividedShowList.add(ArrayList())
+
+            for (i in 0 until dataList.size) {
+                if (cnt == 5) {
+                    dividedShowList.add(ArrayList())
+                    cnt = 0
+                    page++
+                }
+                dividedShowList[page].add(dataList[i])
+                cnt++
+            }
+        }
+
+        val thread = Thread(r)
+        thread.start()
+
+        try {
+            thread.join()
+        } catch (e : InterruptedException){ }
+    }
+
+    fun getShowList(): ArrayList<ArrayList<MyEquipment>> {
+        return dividedShowList
+    }
+}
