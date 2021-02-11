7bdec7d651ad6887ab4fe6d083103cb8623eb5f2
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-11 17:58:06 +0900

    테스트용 이미지를 반환하는 메서드 추가

--- User/app/src/main/java/com/example/jub_jub_android/ui/util/MyUtil.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/util/MyUtil.kt
@@ -11,6 +11,14 @@ import java.io.File
 
 class MyUtil {
 
+    fun getMotorTestImage(context: Context): String{
+        val byteStream = ByteArrayOutputStream()
+        val bitmap = BitmapFactory.decodeResource(context.resources, com.example.jub_jub_android.R.drawable.imageex)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream)
+        val byteArray: ByteArray = byteStream.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
+    }
+
     fun convertBase64ToBitmap(base64Code: String): Bitmap{
         val decodedString: ByteArray = Base64.decode(base64Code, Base64.DEFAULT)
         return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
