46f6f73edacc8bd8574417dd94937b025c0bc9a1
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-14 20:29:37 +0900

    로그인Activity에 권한 확인, 요청 메서드 추가

--- Admin/app/src/main/java/com/example/jub_jub_admin/ui/activity/LoginActivity.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/ui/activity/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.example.jub_jub_admin.ui.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.jub_jub_admin.R
 import com.example.jub_jub_admin.data.remote.NetRetrofit
 import com.example.jub_jub_admin.entity.dataclass.body.Login
@@ -24,6 +28,9 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setPermission()
+
 
         textView_GotoRegister_LoginActivity.setOnClickListener {
             startActivity(Intent(applicationContext, SignUpActivity::class.java))
@@ -51,31 +58,32 @@ class LoginActivity : AppCompatActivity() {
                     response.enqueue(object : Callback<LoginResponse> {
                         override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>)
                         {
-                            if(response.code() == 200){
-                                if(response.isSuccessful){
+                            if(response.isSuccessful){
+
+                                if(response.body()?.success == true){
+
                                     Log.d("TestLog_Login", "로그인 성공!")
                                     Log.d("TestLog_Login", "code = ${response.body()?.code}" +
-                                            "" +
                                             "data = ${response.body()?.data} msg = ${response.body()?.msg} success = ${response.body()?.success} ")
-
                                     TokenManager.setToken(response.body()?.data!!)
                                     //앱 시작
 
                                     Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
                                     startActivity(Intent(applicationContext, MainActivity::class.java))
                                     finish()
-
-                                } else{
+                                }
+                                else{
                                     Toast.makeText(applicationContext, "${response.body()?.msg}", Toast.LENGTH_SHORT).show()
                                 }
+
                             }
-                            else {
-                                Log.d("TestLog_Login", "로그인 실패! ${response.code()}")
+                            else{
+                                Toast.makeText(applicationContext, "${response.body()?.msg}", Toast.LENGTH_SHORT).show()
                             }
                         }
 
                         override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                            Log.d("TestLog_Login", "onFailure ${t.message.toString()}")
+                            Toast.makeText(applicationContext, "${t.message}", Toast.LENGTH_SHORT).show()
                         }
                     })
 
@@ -83,6 +91,36 @@ class LoginActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    private fun setPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "READ 이미 거절 됨",  Toast.LENGTH_SHORT).show()
+                Log.d("TestLog_Login", "READ 이미 거절 됨")
+            }else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
+                Log.d("TestLog_Login", "READ 허용 요청")
+            }
+        }else{
+            Toast.makeText(this, "READ 이미 승인 됨",  Toast.LENGTH_SHORT).show()
+            Log.d("TestLog_Login", "READ 이미 승인 됨")
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "WRITE 이미 거절 됨",  Toast.LENGTH_SHORT).show()
+                Log.d("TestLog_Login", "WRITE 이미 거절 됨")
+            }else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
+                Log.d("TestLog_Login", "WRITE 권한 요청")
+            }
+        }else{
+            Toast.makeText(this, "WRITE 이미 승인 됨",  Toast.LENGTH_SHORT).show()
+            Log.d("TestLog_Login", "WRITE 이미 승인 됨")
+        }
+
     }
 
     override fun onBackPressed() {
