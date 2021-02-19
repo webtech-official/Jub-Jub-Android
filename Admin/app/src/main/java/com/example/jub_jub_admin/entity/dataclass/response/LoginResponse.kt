afaae67d11439c6947de68f07d9fe16e85081adc
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-19 22:43:31 +0900

    로그인 Response Data Class 수정

--- Admin/app/src/main/java/com/example/jub_jub_admin/entity/dataclass/response/LoginResponse.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/entity/dataclass/response/LoginResponse.kt
@@ -2,7 +2,12 @@ package com.example.jub_jub_admin.entity.dataclass.response
 
 data class LoginResponse (
     val code: Int,
-    val data: String,
+    val data: Tokens,
     val msg: String,
     val success: Boolean
+)
+
+data class Tokens (
+    val accessToken: String,
+    val refreshToken: String
 )
