0610a246e8b312d30b989711b7c329e5695f1922
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 22:19:05 +0900

    학생들 기자재 대여 현황 확인하는 Activity 생성

---
+++ Admin/app/src/main/java/com/example/jub_jub_admin/ui/activity/ManageStudentActivity.kt
@@ -0,0 +1,13 @@
+package com.example.jub_jub_admin.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jub_jub_admin.R
+
+class ManageStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_student)
+
+    }
+}
