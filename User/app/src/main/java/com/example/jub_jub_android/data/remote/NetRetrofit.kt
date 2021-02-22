bff979ca66cb624283c62bc7a360a400a7f9f048
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 14:05:52 +0900

    .

--- User/app/src/main/java/com/example/jub_jub_android/data/remote/NetRetrofit.kt
+++ User/app/src/main/java/com/example/jub_jub_android/data/remote/NetRetrofit.kt
@@ -1,28 +1,37 @@
 package com.example.jub_jub_android.data.remote
 
+import com.example.jub_jub_android.entity.singleton.TokenManager
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 object NetRetrofit {
 
-    private val BASE_URL = "http://3.36.29.69:8080/v1/"
+    private val BASE_URL = "http://15.165.97.179:8080/v2/"
+
+    val interceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+
+    var retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     fun getServiceApi() : Api {
-
-        val interceptor = HttpLoggingInterceptor()
-                .setLevel(HttpLoggingInterceptor.Level.BODY)
-
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
-
-        var retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
         return retrofit.create(Api::class.java)
     }
 
+
 }
+
