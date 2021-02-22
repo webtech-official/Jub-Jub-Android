15f2238cc437fb9d187dfdc1e0e7fefe7d2424a5
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 22:16:02 +0900

    .

--- Admin/app/src/main/java/com/example/jub_jub_admin/entity/dataclass/response/GetEquipmentResponse.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/entity/dataclass/response/GetEquipmentResponse.kt
@@ -1,6 +1,8 @@
 package com.example.jub_jub_admin.entity.dataclass.response
 
+import androidx.room.PrimaryKey
 import com.example.jub_jub_admin.entity.dataclass.Equipment
+import com.google.gson.annotations.SerializedName
 
 data class GetEquipmentResponse(
         val code: Int,
