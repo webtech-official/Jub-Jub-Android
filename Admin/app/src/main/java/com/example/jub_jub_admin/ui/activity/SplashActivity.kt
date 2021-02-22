b6d873dfe7770580e4e7e49eff3e567bf30bc8a4
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 22:20:49 +0900

    테스트코드 제거, 자동 로그인 코드 추가

--- Admin/app/src/main/java/com/example/jub_jub_admin/ui/activity/SplashActivity.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/ui/activity/SplashActivity.kt
@@ -1,22 +1,11 @@
 package com.example.jub_jub_admin.ui.activity
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.jub_jub_admin.R
-import com.example.jub_jub_admin.data.remote.NetRetrofit
-import com.example.jub_jub_admin.entity.dataclass.response.ResponseTest
-import com.example.jub_jub_admin.entity.dataclass.response.SearchEquipment
+import com.example.jub_jub_admin.data.local.SharedPref
 import com.example.jub_jub_admin.entity.singleton.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,34 +16,18 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun startApp() {
-
-        StudentRentStatusListManager.setDummyDataList(applicationContext, 100)
-        ManageLaptopListManager.setDummyData(applicationContext)
-
-        RentRequestListManager.setDummyData(applicationContext)
-
-        val response: Call<ResponseTest> = NetRetrofit.getServiceApi().getTest()
-
-        response.enqueue(object: Callback<ResponseTest>{
-            override fun onResponse(call: Call<ResponseTest>, response: Response<ResponseTest>) {
-                Log.d("TestLog", "onResponse!")
-                if(response.code() == 200){
-                    Log.d("TestLog", "body.msg = ${response.body()?.msg}")
-
-                }else{
-                    Log.d("TestLog", "${response.code()}")
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseTest>, t: Throwable) {
-                Log.d("TestLog", t.message.toString())
-                Log.d("TestLog", "Fail!")
-            }
-        })
-
-        startActivity(Intent(applicationContext, LoginActivity ::class.java))
+        //MyEquipmentListManager.setDummyDataList(applicationContext, 100)
+        val intent = Intent(applicationContext, LoginActivity ::class.java)
+        init(intent)
+        startActivity(intent)
         finish()
     }
 
+    private fun init(intent: Intent) {
+        val sharedPref = SharedPref(applicationContext)
 
+        if(sharedPref.isExist("Id")){
+            intent.putExtra("LoginData",  sharedPref.getAccount())
+        }
+    }
 }
