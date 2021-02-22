b55ce4016babf0dd01ca84bc82fe32cfcfe90fa3
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 22:22:52 +0900

    PageView DataClass 변경

--- Admin/app/src/main/java/com/example/jub_jub_admin/ui/util/PageView/AllowRentRequestList_PageView.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/ui/util/PageView/AllowRentRequestList_PageView.kt
@@ -2,13 +2,13 @@ package com.example.jub_jub_admin.ui.util.PageView
 
 import android.content.Context
 import android.view.View
-import com.example.jub_jub_admin.entity.dataclass.RentRequest
+import com.example.jub_jub_admin.entity.dataclass.response.RentRequestDetailInfo
 import com.example.jub_jub_admin.entity.singleton.RentRequestListManager
 import com.example.jub_jub_admin.ui.adapter.viewpager.AllowRentRequest_ViewPagerAdapter
 import com.example.jub_jub_admin.ui.util.SetPageView
 import kotlinx.android.synthetic.main.layout_pageview.view.*
 
-class AllowRentRequestList_PageView(var context: Context, var view: View, var  dataList: ArrayList<ArrayList<RentRequest>>) {
+class AllowRentRequestList_PageView(var context: Context, var view: View, var dataList: ArrayList<ArrayList<RentRequestDetailInfo>>) {
 
     private lateinit var setPageView : SetPageView
 
