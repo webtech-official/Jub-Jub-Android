e58be01cceed4284a7db0e01c12a829e84061c06
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 22:19:38 +0900

    회원 가입 Activity에 로딩 바 생성

--- Admin/app/src/main/java/com/example/jub_jub_admin/ui/activity/SignUpActivity.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/ui/activity/SignUpActivity.kt
@@ -3,11 +3,12 @@ package com.example.jub_jub_admin.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.example.jub_jub_admin.R
 import com.example.jub_jub_admin.data.remote.NetRetrofit
 import com.example.jub_jub_admin.entity.dataclass.body.SignUp
-import com.example.jub_jub_admin.entity.dataclass.response.SignUpResponse
+import com.example.jub_jub_admin.entity.dataclass.response.MyResponse
 import kotlinx.android.synthetic.main.activity_signup.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,44 +22,44 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
 
         button_SignUp_SignUpActivity.setOnClickListener {
-
             if (checkEditText()) {
+                progress_bar.visibility = View.VISIBLE
+                signUp()
+            }
+        }
+    }
 
-                val signUpData = SignUp(
-                    editText_ClassNum_SignUpActivity.text.toString(),
-                    editText_Email_SignUpActivity.text.toString(),
+    private fun signUp(){
+        val signUpData = SignUp(
+                editText_ClassNum_SignUpActivity.text.toString(),
+                editText_Email_SignUpActivity.text.toString(),
+                editText_Name_SignUpActivity.text.toString(),
+                editText_Password_SignUpActivity.text.toString()
+        )
+        val response: Call<MyResponse> = NetRetrofit.getServiceApi().signUp(signUpData)
 
-                    editText_Name_SignUpActivity.text.toString(),
-                    editText_Password_SignUpActivity.text.toString()
-                )
+        response.enqueue(object : Callback<MyResponse> {
 
-                val response: Call<SignUpResponse> = NetRetrofit.getServiceApi().signUp(signUpData)
-
-                response.enqueue(object : Callback<SignUpResponse> {
-                    override fun onResponse(
-                        call: Call<SignUpResponse>,
-                        response: Response<SignUpResponse>
-                    ) {
-                        Log.d("TestLog", "onResponse!")
-                        if (response.code() == 200) {
-                            Log.d("TestLog", "success = ${response.body()?.success}")
-                            Toast.makeText(applicationContext, "회원가입 완료!", Toast.LENGTH_SHORT)
-                                .show()
-                            finish()
-                        } else {
-                            Log.d("TestLog", "${response.code()}")
-                        }
-                    }
-
-                    override fun onFailure(call: Call<SignUpResponse>, t: Throwable) {
-                        Log.d("TestLog", "onFailure! ${t.message}")
-                    }
-                })
+            override fun onResponse(call: Call<MyResponse>, response: Response<MyResponse>) {
+                if (response.body()?.success!!) {
+                    Toast.makeText(applicationContext, "계정 생성 완료! 이메일을 확인해주세요!", Toast.LENGTH_SHORT).show()
+                    progress_bar.visibility = View.VISIBLE
+                    finish()
+                } else {
+                    Log.d("TestLog", "${response.code()}")
+                    Toast.makeText(applicationContext, response.body()?.msg, Toast.LENGTH_SHORT).show()
+                }
             }
 
-        }
-
+            override fun onFailure(call: Call<MyResponse>, t: Throwable) {
+                Toast.makeText(applicationContext, "서버 연결 실패", Toast.LENGTH_SHORT).show()
+                Log.d("TestLog", "onFailure! ${t.message}")
+                progress_bar.visibility = View.VISIBLE
+            }
+        })
     }
+
+
     private fun checkEditText(): Boolean{
 
         return if(editText_ClassNum_SignUpActivity.text.toString() == "" || editText_Email_SignUpActivity.text.toString() == "" || editText_Name_SignUpActivity.text.toString() == ""
@@ -66,7 +67,7 @@ class SignUpActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, "빈칸을 모두 입력해주세요!", Toast.LENGTH_SHORT).show()
             false
         } else if(editText_Password_SignUpActivity.text.toString() != editText_PasswordCheck_SignUpActivity.text.toString()){
-            Toast.makeText(applicationContext, "Password와 Password check의 값이 달라요! ", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Password와 Password check의 값이 다릅니다", Toast.LENGTH_SHORT).show()
             false
         } else{
             Log.d("TestLog", "checkEditText 통과")
