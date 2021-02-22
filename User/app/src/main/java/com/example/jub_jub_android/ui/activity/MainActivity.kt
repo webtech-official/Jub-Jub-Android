5eba41759924b89566eb0498cde8037f9522dfb4
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 14:25:58 +0900

    타이틀 바 Search Mode일 때 뒤로가기 누르면 ViewMode로 변경

--- User/app/src/main/java/com/example/jub_jub_android/ui/activity/MainActivity.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/activity/MainActivity.kt
@@ -23,6 +23,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,14 +32,14 @@ class MainActivity : AppCompatActivity() {
     private var lastSize = 0
     //마지막으로 뒤로가기 버튼 누른 시간
     var backKeyPressedTime : Long = 0
+    var isViewMode = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("TestLog", "데이터 가져오기 시작")
+        //test()
         getDataFromServer()
-        Log.d("TestLog", "데이터 가져오기 pageView Setting 시작")
 
         setTitleBarItemsListener()
 
@@ -57,24 +58,30 @@ class MainActivity : AppCompatActivity() {
             pageView.initViewPager()
     }
 
+
     private fun getDataFromServer() {
+        Log.d("TestLog_Main", "Token = ${TokenManager.getToken()}")
 
         val response: Call<EquipmentResponse> = NetRetrofit.getServiceApi().getEquipmentData(TokenManager.getToken())
 
         response.enqueue(object :Callback<EquipmentResponse> {
             override fun onResponse(call: Call<EquipmentResponse>, response: Response<EquipmentResponse>) {
+                Log.d("TestLog_Main", "isSuc = ${response.isSuccessful}")
+                Log.d("TestLog_Main", "re.body.suc = ${response.body()?.success}")
                 if(response.body()!!.success){
                     setDataList(applicationContext, response.body()?.list!!)
                     setPageView()
                     Log.d("TestLog_Main", "DataList.size = ${ItemStatusListManager.getShowList().size}")
                 }
                 else{
+                    Log.d("TestLog_Main", "body = ${response.body()}")
                     Toast.makeText(applicationContext, "실패했습니다!", Toast.LENGTH_SHORT).show()
                 }
             }
 
             override fun onFailure(call: Call<EquipmentResponse>, t: Throwable) {
                 Log.d("TestLog",  "Fail! ${t.message}")
+                Log.d("TestLog",  "Fail! ${t.localizedMessage}")
             }
 
         })
@@ -102,9 +109,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setSearchFunction() {
         editText_SearchText_MainActivitySearchMode.addTextChangedListener {
-            Log.d("TestLog_Main", "text = ${editText_SearchText_MainActivitySearchMode.text}")
-            search(it.toString().toLowerCase(Locale.getDefault()).replace(" ", ""))
 
+            Log.d("TestLog_Main", "text = ${editText_SearchText_MainActivitySearchMode.text}")
+
+            search(it.toString().toLowerCase(Locale.getDefault()).replace(" ", ""))
 
             //이거 검색 최적화 알고리즘 수정 해야함
             var currentSize = ItemStatusListManager.getShowList()[0].size
@@ -132,11 +140,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setTitleBarSearchMode() {
+        isViewMode = false
         titleBar_ViewMode_MainActivity.visibility = View.GONE
         titleBar_SearchMode_MainActivity.visibility = View.VISIBLE
     }
 
     private fun setTitleBarViewMode() {
+        isViewMode = true
         titleBar_ViewMode_MainActivity.visibility = View.VISIBLE
         titleBar_SearchMode_MainActivity.visibility = View.GONE
         search("")
@@ -146,14 +156,18 @@ class MainActivity : AppCompatActivity() {
     //뒤로가기 버튼 눌렀을 때
     override fun onBackPressed() {
 
-        //1번 눌렀을 때
-        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
-            backKeyPressedTime = System.currentTimeMillis()
-            Toast.makeText(applicationContext, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
-        }
-        //2초 안에 2번 눌렀을 때 종료
-        else if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
-            finishApp()
+        if(!isViewMode){
+            setTitleBarViewMode()
+        }else{
+            //1번 눌렀을 때
+            if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+                backKeyPressedTime = System.currentTimeMillis()
+                Toast.makeText(applicationContext, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            }
+            //2초 안에 2번 눌렀을 때 종료
+            else if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+                finishApp()
+            }
         }
     }
 
