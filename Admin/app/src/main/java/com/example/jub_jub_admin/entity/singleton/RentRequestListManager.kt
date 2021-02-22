fe67f5988c9ad47878f7bf280b0627c20b67461b
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 22:17:29 +0900

    대여신청 ListManager에 setDataList추가, 불필요한 것 같아 DB 사용하는 코드 제거

--- Admin/app/src/main/java/com/example/jub_jub_admin/entity/singleton/RentRequestListManager.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/entity/singleton/RentRequestListManager.kt
@@ -3,70 +3,38 @@ package com.example.jub_jub_admin.entity.singleton
 import android.content.Context
 import com.example.jub_jub_admin.data.local.db.RentRequestDB
 import com.example.jub_jub_admin.entity.dataclass.RentRequest
+import com.example.jub_jub_admin.entity.dataclass.response.RentRequestDetailInfo
 import com.example.jub_jub_admin.ui.util.MyUtil
 
 object RentRequestListManager {
     private lateinit var rentRequestDB: RentRequestDB
 
-    private var dividedShowList = ArrayList<ArrayList<RentRequest>>()
+    private var dividedShowList = ArrayList<ArrayList<RentRequestDetailInfo>>()
 
-    fun setDummyData(context: Context){
-        rentRequestDB = RentRequestDB.getInstance(context)!!
+    private var dataList =  ArrayList<RentRequestDetailInfo>()
 
-
-        var r = Runnable {
-
-            val baseString = MyUtil.getMotorTestImage(context)
-
-            rentRequestDB.rentRequestDAO().clear()
-            var cnt = 1
-            for(i in 0..10){
-                rentRequestDB.rentRequestDAO().insert(RentRequest("DC모터", baseString, "모터" , 2205, cnt++))
-                rentRequestDB.rentRequestDAO().insert(RentRequest("블루투스 무선 CK96 키보드", baseString, "키보드" , 2204, cnt++))
-
-            }
-        }
-
-        val thread = Thread(r)
-        thread.start()
-
-        try {
-            thread.join()
-        } catch (e : InterruptedException){ }
-
-        processShowList("")
-
+    fun setDataList(dataList: ArrayList<RentRequestDetailInfo>){
+        this.dataList = dataList
+        processShowList()
     }
 
-    fun processShowList(key: String){
-        var dataList = ArrayList<RentRequest>()
-
-
+    fun processShowList(){
         var r = Runnable {
-            if(key == ""){
-                dataList = rentRequestDB.rentRequestDAO().getAll() as ArrayList<RentRequest>
-                //Log.d("TestLog", " withoutkey ${dataList.size}")
-                //Log.d("TestLog", "Key = \"\" , getoriginalDList = ${getOriginalDividedRentRequestList().size} ")
-                //Log.d("TestLog", "Key = \"\" , originalDList = ${originalDividedRentRequestList.size} ")
-            }
-            else {
-                dataList = rentRequestDB.rentRequestDAO().search("%$key%") as ArrayList<RentRequest>
-                //Log.d("TestLog", " $key = ${dataList.size}")
-            }
-
             dividedShowList.clear()
             var page = 0
             var cnt = 0
             dividedShowList.add(ArrayList())
 
             for (i in 0 until dataList.size) {
-                if (cnt == 5) {
-                    dividedShowList.add(ArrayList())
-                    cnt = 0
-                    page++
+                if(dataList[i].equipmentEnum == "ROLE_Waiting"){
+                    if (cnt == 5) {
+                        dividedShowList.add(ArrayList())
+                        cnt = 0
+                        page++
+                    }
+                    dividedShowList[page].add(dataList[i])
+                    cnt++
                 }
-                dividedShowList[page].add(dataList[i])
-                cnt++
             }
 
         }
@@ -79,7 +47,7 @@ object RentRequestListManager {
 
     }
 
-    fun getShowList(): ArrayList<ArrayList<RentRequest>>{
+    fun getShowList(): ArrayList<ArrayList<RentRequestDetailInfo>> {
         return dividedShowList
     }
 
