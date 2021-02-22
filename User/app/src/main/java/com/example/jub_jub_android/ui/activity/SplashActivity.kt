a593d02767569d0da601abdcf7d8c198ae775172
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-02-22 14:29:10 +0900

    SplashActivity에서 LoginActivity().login(loginData)해서 자동 로그인 하려고 했는데 context관련 에러가 생겨서 intent.putExtra로 자동 로그인 추가

--- User/app/src/main/java/com/example/jub_jub_android/ui/activity/SplashActivity.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/activity/SplashActivity.kt
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.jub_jub_android.R
+import com.example.jub_jub_android.data.local.SharedPref
 import com.example.jub_jub_android.data.remote.NetRetrofit
 import com.example.jub_jub_android.entity.singleton.MyEquipmentListManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.Serializable
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var sharedPref: SharedPref
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -20,10 +23,18 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun startApp() {
-
          //MyEquipmentListManager.setDummyDataList(applicationContext, 100)
-
-        startActivity(Intent(applicationContext, LoginActivity ::class.java))
+        val intent = Intent(applicationContext, LoginActivity ::class.java)
+        init(intent)
+        startActivity(intent)
         finish()
+    }
+
+    private fun init(intent: Intent) {
+        sharedPref = SharedPref(applicationContext)
+
+        if(sharedPref.isExist("Id")){
+            intent.putExtra("LoginData",  sharedPref.getAccount())
+        }
     }
 }
