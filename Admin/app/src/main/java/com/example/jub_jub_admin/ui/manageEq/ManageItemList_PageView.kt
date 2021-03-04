bd0f9466eb5ed38ce7d7d464e274c49124c3da30
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-03-04 22:46:52 +0900

    .

--- Admin/app/src/main/java/com/example/jub_jub_admin/ui/manageEq/ManageItemList_PageView.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/ui/manageEq/ManageItemList_PageView.kt
@@ -1,13 +1,12 @@
 package com.example.jub_jub_admin.ui.manageEq
 
 import android.content.Context
+import android.util.Log
 import android.view.View
-import com.example.jub_jub_admin.entity.dataclass.Equipment
-import com.example.jub_jub_admin.entity.singleton.EquipmentManager
 import com.example.jub_jub_admin.ui.util.SetPageView
 import kotlinx.android.synthetic.main.layout_pageview.view.*
 
-class ManageItemList_PageView(var context: Context, var view: View, var  dataList: ArrayList<ArrayList<Equipment>>){
+class ManageItemList_PageView(var context: Context, var view: View, var viewModel: ManageEquipmentViewModel){
 
     private lateinit var setPageView : SetPageView
 
@@ -15,20 +14,15 @@ class ManageItemList_PageView(var context: Context, var view: View, var  dataLis
 
         val viewPager = view.viewPager
 
-        viewPager.adapter = ManageItem_ViewPagerAdapter(context)
+        viewPager.adapter = ManageItem_ViewPagerAdapter(context,viewModel)
 
-        setPageView = SetPageView(view, viewPager, dataList as ArrayList<ArrayList<Any>>)
-
+        setPageView = SetPageView(view, viewPager, viewModel.getShowList() as ArrayList<ArrayList<Any>>)
     }
 
     fun syncPage(){
         //Log.d("TestLog", "ISList.size = ${ItemStatusListManager.getShowList().size}")
-        setPageView.syncPage(EquipmentManager.getShowList().size)
+        setPageView.syncPage(viewModel.getShowList().size)
     }
-
-
-
-
 
 }
 
