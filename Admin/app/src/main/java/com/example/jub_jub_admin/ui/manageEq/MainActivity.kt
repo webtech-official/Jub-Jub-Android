77d24151fc409426e999e48f4b2323042b783ab8
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-03-04 22:36:47 +0900

    MainActivity ViewModel 추가

--- Admin/app/src/main/java/com/example/jub_jub_admin/ui/manageEq/MainActivity.kt
+++ Admin/app/src/main/java/com/example/jub_jub_admin/ui/manageEq/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jub_jub_admin.ui.manageEq
 
 import android.content.Intent
+import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import android.os.Process
 import android.util.Log
@@ -8,23 +9,21 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
 import com.example.jub_jub_admin.R
-import com.example.jub_jub_admin.data.remote.NetRetrofit
-import com.example.jub_jub_admin.entity.dataclass.response.GetEquipmentResponse
-import com.example.jub_jub_admin.entity.singleton.EquipmentManager
 import com.example.jub_jub_admin.entity.singleton.TokenManager
 import com.example.jub_jub_admin.ui.activity.MyPageActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_pageview.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    private val MODIFY_EQUIPMENT = 1042
+
+    private lateinit var viewModel: ManageEquipmentViewModel
+
     private lateinit var pageView: ManageItemList_PageView
+
     private var lastSize = 0
     //마지막으로 뒤로가기 버튼 누른 시간
     var backKeyPressedTime : Long = 0
@@ -33,60 +32,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getDataFromServer()
+        viewModel = ViewModelProvider(this).get(ManageEquipmentViewModel::class.java)
+        viewModel.init(applicationContext)
+
+
+        //viewModel.getDataFromServer(true)
 
         setTitleBarItemsListener()
+
         refreshLayout.setOnRefreshListener {
             refresh()
         }
-    }
-    private fun refresh(){
-        refreshLayout.isRefreshing = true
-        getDataFromServer()
-        pageView.syncPage()
-        Log.d("TestLog", "새로고침 완료!")
-        refreshLayout.isRefreshing = false
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Log.d("TestLog_MainAc", "result = $resultCode, request = $requestCode")
-        if(resultCode == RESULT_OK && requestCode == MODIFY_EQUIPMENT){
-            Log.d("TestLog_MainAc", "새로고침 완료!")
-            refresh()
-        }
-    }
+        ManageEquipmentViewModel.i.observe(this, {
+            viewModel.getDataFromServer()
+        })
 
-    private fun getDataFromServer() {
-        Log.d("TestLog_MainAc", "Token = ${TokenManager.getToken()}")
-        val response: Call<GetEquipmentResponse> = NetRetrofit.getServiceApi().getAllEquipment(TokenManager.getToken())
-
-        response.enqueue(object: Callback<GetEquipmentResponse> {
-            override fun onResponse(call: Call<GetEquipmentResponse>, response: Response<GetEquipmentResponse>) {
-
-                if(response.body()?.success == true){
-                    Log.d("TestLog_MainAc", "Success! list.size = ${response.body()?.list?.size}")
-                    EquipmentManager.setDataList(applicationContext, response.body()?.list!!)
-                    setPageView()
-
-                }else{
-                    Log.d("TestLog_mainAc", "Fail! ${response.body()?.msg}")
-                }
-
-            }
-
-            override fun onFailure(call: Call<GetEquipmentResponse>, t: Throwable) {
-                Log.d("TestLog_mainAc", "Fail! ${t.message}")
-            }
+        ManageEquipmentViewModel.list.observe(this, {
+            pageView.syncPage()
         })
     }
 
+    //region setPageView()
     private fun setPageView(){
-        pageView = ManageItemList_PageView(applicationContext, pageView_MainActivity, EquipmentManager.getShowList())
+        pageView = ManageItemList_PageView(applicationContext, pageView_MainActivity, viewModel)
         pageView.initViewPager()
     }
+    //endregion
 
-    //타이틀 바 위젯들 온클릭 등록
+    //region 새로고침
+    private fun refresh(){
+        refreshLayout.isRefreshing = true
+        viewModel.getDataFromServer()
+        Log.d("TestLog", "새로고침 완료!")
+        refreshLayout.isRefreshing = false
+    }
+    //endregion
+
+    //region 타이틀 바 위젯들 온클릭 등록
     private fun setTitleBarItemsListener() {
 
         imageView_SearchMode_MainActivity.setOnClickListener {
@@ -101,29 +84,32 @@ class MainActivity : AppCompatActivity() {
             editText_SearchText_MainActivitySearchMode.setText("")
             setTitleBarViewMode()
         }
-
+        //검색 기능 등록
         setSearchFunction()
     }
+    //endregion
 
+    //region EditText.changeListener에 검색 기능 등록
     private fun setSearchFunction() {
         editText_SearchText_MainActivitySearchMode.addTextChangedListener {
 
             search(it.toString().toLowerCase(Locale.getDefault()).replace(" ", ""))
 
-            var currentSize = EquipmentManager.getShowList().size
-
+            var currentSize = viewModel.getShowList().size
 
             if(lastSize != currentSize){
                 pageView.syncPage()
                 lastSize = currentSize
             }
 
-
         }
     }
+    //endregion
+
+    //region 검색
     private fun search(word: String){
         var r = Runnable {
-            EquipmentManager.processShowList(word)
+            viewModel.processShowList(word)
         }
 
         val thread = Thread(r)
@@ -133,20 +119,26 @@ class MainActivity : AppCompatActivity() {
             thread.join()
         } catch (e : InterruptedException){ }
     }
+    //endregion
 
+    //region 타이틀 바 검색모드
     private fun setTitleBarSearchMode() {
         titleBar_ViewMode_MainActivity.visibility = View.GONE
         titleBar_SearchMode_MainActivity.visibility = View.VISIBLE
     }
+    //endregion
 
+    //region 타이틀 바 뷰 모드
     private fun setTitleBarViewMode() {
         titleBar_ViewMode_MainActivity.visibility = View.VISIBLE
         titleBar_SearchMode_MainActivity.visibility = View.GONE
         search("")
         pageView.syncPage()
-    }
 
-    //뒤로가기 버튼 눌렀을 때
+    }
+    //endregion
+
+    //region 뒤로가기 버튼 눌렀을 때
     override fun onBackPressed() {
 
         //1번 눌렀을 때
@@ -159,11 +151,14 @@ class MainActivity : AppCompatActivity() {
             finishApp()
         }
     }
+    //endregion
 
+    //region 앱 완전 종료
     private fun finishApp(){
         moveTaskToBack(true)                        // 태스크를 백그라운드로 이동
         finishAndRemoveTask()                        // 액티비티 종료 + 태스크 리스트에서 지우기
         Process.killProcess(Process.myPid())    // 앱 프로세스 종료
     }
+    //endregion
 
 }
