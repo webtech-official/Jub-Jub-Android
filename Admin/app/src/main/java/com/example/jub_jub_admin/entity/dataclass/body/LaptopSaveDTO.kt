3df8ae496e2defdf06c193b61bfde81a68dd3fd4
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-03-05 21:05:03 +0900

    노트북 추가할 때 Body에 보낼 Data Class 생성

---
+++ Admin/app/src/main/java/com/example/jub_jub_admin/entity/dataclass/body/LaptopSaveDTO.kt
@@ -0,0 +1,8 @@
+package com.example.jub_jub_admin.entity.dataclass.body
+
+data class LaptopSaveDTO(
+        val classNumber: String,
+        val laptopSerialNumber: String,
+        val specIdx: Int,
+        val studentName: String
+)
