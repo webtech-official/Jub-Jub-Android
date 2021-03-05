cbe8e0dcc64ab01eaa95f5ce0a82310260e5752f
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-03-05 21:04:07 +0900

    노트북 사양 추가할 때 Body에 보낼 Data Class 생성

---
+++ Admin/app/src/main/java/com/example/jub_jub_admin/entity/dataclass/body/AddLaptopSpec.kt
@@ -0,0 +1,11 @@
+package com.example.jub_jub_admin.entity.dataclass.body
+
+data class AddLaptopSpec(
+    val cpu: String,
+    val gpu : String,
+    val hdd: String,
+    val laptopBrand: String,
+    val laptopName: String,
+    val ram: String,
+    val ssd: String,
+)
