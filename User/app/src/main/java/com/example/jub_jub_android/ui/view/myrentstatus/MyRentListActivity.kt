38621a09c97ee4aaf68d427367caef0a01788c8d
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-03-11 17:17:47 +0900

    나의 대여 목록 Activity에 DataBinding과 ViewModel 적용

--- User/app/src/main/java/com/example/jub_jub_android/ui/view/myrentstatus/MyRentListActivity.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/view/myrentstatus/MyRentListActivity.kt
@@ -2,108 +2,74 @@ package com.example.jub_jub_android.ui.view.myrentstatus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.jub_jub_android.R
-import com.example.jub_jub_android.data.remote.NetRetrofit
-import com.example.jub_jub_android.entity.dataclass.response.MyEquipmentResponse
-import com.example.jub_jub_android.entity.singleton.MyEquipmentListManager
-import com.example.jub_jub_android.entity.singleton.MyEquipmentListManager.setDataList
-import com.example.jub_jub_android.entity.singleton.TokenManager
+import com.example.jub_jub_android.databinding.ActivityMyRentalListBinding
 import kotlinx.android.synthetic.main.activity_my_rental_list.*
 import kotlinx.android.synthetic.main.layout_pageview.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class MyRentListActivity : AppCompatActivity() {
 
     lateinit var pageView : MyRentList_PageView
+
+    lateinit var viewModel: MyRentList_ViewModel
+
+    lateinit var binding: ActivityMyRentalListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_rental_list)
 
-        getDataFromServer()
+        init()
 
-        setTitleBarItemsOnclick()
+    }
+
+    private fun init() {
+
+        viewModel = ViewModelProvider(this).get(MyRentList_ViewModel::class.java)
+        viewModel.init(applicationContext)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_my_rental_list)
+        binding.activity = this
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        setPageView()
 
         refreshLayout.setOnRefreshListener {
-            getDataFromServer()
-
-            pageView.syncPage()
-            Toast.makeText(applicationContext, "새로고침 완료", Toast.LENGTH_SHORT).show()
-            refreshLayout.isRefreshing = false
+            refresh()
         }
+
+        viewModel.showStatus.observe(this, {
+            viewModel.processShowList(it)
+        })
+
+        viewModel.list.observe(this, {
+            pageView.syncPage()
+        })
+
+    }
+
+    private fun refresh() {
+        refreshLayout.isRefreshing = true
+        viewModel.getMyEquipmentData(applicationContext)
+        Toast.makeText(applicationContext, "새로고침 완료", Toast.LENGTH_SHORT).show()
+        refreshLayout.isRefreshing = false
     }
 
     private fun setPageView() {
-        pageView = MyRentList_PageView(applicationContext, pageView_MyRentalListActivity, MyEquipmentListManager.getShowList())
+        pageView = MyRentList_PageView(applicationContext, pageView_MyRentalListActivity, viewModel)
         pageView.initViewPager()
     }
 
-    private fun getDataFromServer() {
-        val response: Call<MyEquipmentResponse> = NetRetrofit.getServiceApi().getMyEquipmentData(TokenManager.getToken())
 
-        response.enqueue(object: Callback<MyEquipmentResponse>{
-            override fun onResponse(
-                call: Call<MyEquipmentResponse>,
-                response: Response<MyEquipmentResponse>
-            ) {
-                if(response.body()?.success!!){
-                    setDataList(applicationContext, response.body()?.list!!)
-                    setPageView()
-                    MyEquipmentListManager.processShowList("${textView_ShowMode_MyRentalListActivity.text}")
-
-                }else{
-                    Toast.makeText(applicationContext, "서버에서 데이터를 받아오는데 실패했습니다!", Toast.LENGTH_SHORT).show()
-                }
-                Log.d("TestLog_MyEquipList", "isSuc = ${response.isSuccessful}")
-                Log.d("TestLog_MyEquipList", "size = ${response.body()?.list?.size}")
-
-            }
-
-            override fun onFailure(call: Call<MyEquipmentResponse>, t: Throwable) {
-                Log.d("TestLog_MyEquipList", "t = ${t}")
-                Log.d("TestLog_MyEquipList", "t.localizedMessage = ${t.localizedMessage}")
-                Log.d("TestLog_MyEquipList", "call = ${call}")
-            }
-        })
-    }
-
-    private fun setTitleBarItemsOnclick() {
-
-        imageView_BackArrow_MyRentalListActivity.setOnClickListener {
-            back()
-        }
-
-        textView_ShowMode_MyRentalListActivity.setOnClickListener {
-            when(textView_ShowMode_MyRentalListActivity.text.toString()){
-                "전체" -> setShowMode("대기")
-                "대기" -> setShowMode("대여")
-                "대여" -> setShowMode("승인")
-                "승인" -> setShowMode("반납")
-                "반납" -> setShowMode("거절")
-                "거절" -> setShowMode("전체")
-            }
-
-            //RentAdapter.getViewPagerAdapter().notifyDataSetChanged()
-            //RentAdapter.getRecyclerAdapter().notifyDataSetChanged()
-            pageView.syncPage()
-        }
-    }
-    private fun setShowMode(text: String){
-        textView_ShowMode_MyRentalListActivity.text = text
-        MyEquipmentListManager.processShowList(text)
-    }
-
-    private fun back(){
-        MyEquipmentListManager.processShowList("전체")
-        pageView.syncPage()
+    fun back(){
+        viewModel.processShowList("전체")
         finish()
     }
 
-    override fun onBackPressed() {
-        back()
-    }
+    override fun onBackPressed() = back()
 }
