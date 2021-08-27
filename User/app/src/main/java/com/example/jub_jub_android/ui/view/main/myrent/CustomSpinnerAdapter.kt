a9853675f6e37dd12f38f3e32d5fe463dbabcb6b
Hotteock <57486593+Hotteock3388@users.noreply.github.com>
2021-08-27 22:26:04 +0900

    chore: set Ripple onClick Effect at  every border view without spinner dropdown items
    Spinner dropdown 부분 외에 bg_border가 적용된 모든 View의 onClick 효과에 Ripple 효과 적용 완료

--- User/app/src/main/java/com/example/jub_jub_android/ui/view/main/myrent/CustomSpinnerAdapter.kt
+++ User/app/src/main/java/com/example/jub_jub_android/ui/view/main/myrent/CustomSpinnerAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.jub_jub_android.ui.view.main.myrent
 
 import android.content.Context
+import android.content.res.Resources
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.example.jub_jub_android.R
 
 class CustomSpinnerAdapter(private val con: Context, private val itemLayoutId: Int, private val dataList: Array<String>) : ArrayAdapter<String>(con, itemLayoutId, dataList){
@@ -27,6 +30,7 @@ class CustomSpinnerAdapter(private val con: Context, private val itemLayoutId: I
         val v = LayoutInflater.from(con).inflate(R.layout.layout_rent_status_current_item, parent, false)
 
         v.setBackgroundResource(R.drawable.bg_spinner)
+        v.foreground = null
         Log.d("TestLog", "$v")
         //v.findViewById<TextView>(R.id.textView_RentStatus_Item).text = dataList[position]
                 //super.getView(position, convertView, parent)
@@ -37,6 +41,14 @@ class CustomSpinnerAdapter(private val con: Context, private val itemLayoutId: I
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = LayoutInflater.from(con).inflate(itemLayoutId, parent,false)
         view.findViewById<TextView>(R.id.textView_RentStatus_Item).text = dataList[position]
+
+        if(position == 0){
+            view.background = ContextCompat.getDrawable(context, R.drawable.bg_top_spinner_item)
+            view.foreground = null
+        }else if (position == dataList.size){
+            view.background = ContextCompat.getDrawable(context, R.drawable.bg_bottom_spinner_item)
+            view.foreground = null
+        }
         Log.d("TestLog", "$view")
         return view
     }
